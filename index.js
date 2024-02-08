64b310df65515126c14c0e15d86da157264336e7
Temur <shernazarovtemurcom001@gmail.com>
2024-02-08 15:54:10 +0500

    fixed

--- index.js
+++
@@ -1,2 +0,0 @@
-const a = "Hello Redux";
-console.log(a);
---
+++ main.js
@@ -0,0 +1,50 @@
+// Define actions
+const ADD_TODO = "ADD_TODO";
+
+// Define action creators
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    payload: todo,
+  };
+}
+
+// Define reducer
+function todoReducer(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
+
+// Create store
+const store = Redux.createStore(todoReducer);
+
+// Subscribe to store changes
+store.subscribe(() => {
+  const todos = store.getState();
+  renderTodos(todos);
+});
+
+// Render todos
+function renderTodos(todos) {
+  const todoList = document.getElementById("todoList");
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    const span = document.createElement("span");
+    span.textContent = todo;
+    todoList.appendChild(span);
+  });
+}
+
+// Add todo function
+function addTodo() {
+  const input = document.getElementById("todoInput");
+  const todoText = input.value.trim();
+  if (todoText !== "") {
+    store.dispatch(addTodoAction(todoText));
+    input.value = "";
+  }
+}
